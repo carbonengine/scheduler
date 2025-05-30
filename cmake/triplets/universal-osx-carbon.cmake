f023bb7b32b414c9651d66308e1c132ccff7f472
ccptoebeans <>
2025-05-30 09:49:21 +0000

    force release version of greenlet

--- cmake/triplets/universal-osx-carbon.cmake
+++ cmake/triplets/universal-osx-carbon.cmake
@@ -3,3 +3,7 @@ set(VCPKG_LIBRARY_LINKAGE dynamic)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES "arm64;x86_64")
+
+if (PORT MATCHES "greenlet")
+    set(VCPKG_BUILD_TYPE release)
+endif()
