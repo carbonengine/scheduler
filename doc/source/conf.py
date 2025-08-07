716f4f8c7d7ecb08eb93c60857765b5ddc02b00f
ccp-serpent <alexanderb@ccpgames.com>
2025-08-07 12:48:12 +0000

    Remove `sys.path` insertion from Sphinx configuration
    Cases this particular code was guarding against should be
    covered by the `PYTHONPATH` information passed to the python
    executable during generation.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,9 +1,3 @@
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here.
-import pathlib
-import sys
-sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
-
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
